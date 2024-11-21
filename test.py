caee36b99f9193532dd8bc0d0625021287d36e0e
ikm216 <ikmclassof22@gmail.com>
2024-11-21 00:01:58 -0500

    doc: added documentation for add

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 def add(num1, num2):
-    
+    """ Adds two numbers and returns the result
+
+    Args:
+        num1: the first summand
+        num2: the second summand
+
+    Returns:
+        The sum of the two numbers
+    """
     return num1 + num2
 
 def test_should_return_four_for_two_and_two():
